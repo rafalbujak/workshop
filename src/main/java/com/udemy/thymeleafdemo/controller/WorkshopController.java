e64d968036d398037898bf2a84373347f92202d8
bujak.rafal@yahoo.com <bujak.rafal@yahoo.com>
2019-03-29 19:11:40 +0100

    added form, method and mapinng to add object to DB

--- src/main/java/com/udemy/thymeleafdemo/controller/WorkshopController.java
+++ src/main/java/com/udemy/thymeleafdemo/controller/WorkshopController.java
@@ -4,9 +4,13 @@ import com.udemy.thymeleafdemo.entity.Vehicle;
 import com.udemy.thymeleafdemo.service.VehicleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -26,6 +30,31 @@ public class WorkshopController {
 
         theModel.addAttribute("vehicles", theVehicles);
 
-        return "list-vehicles";
+        return "vehicles/list-vehicles";
+    }
+
+    @GetMapping("/showFormForAddVehicle")
+    public String showFormForAddVehicle(Model model) {
+        Vehicle theVehicle = new Vehicle();
+
+        model.addAttribute("vehicle", theVehicle);
+        return "vehicles/vehicle-form";
+    }
+
+    @GetMapping("/save")
+    public String saveVehicle(Model model) {
+        model.addAttribute("vehicle", new Vehicle());
+        return "vehicles/vehicle-form";
+    }
+
+    @PostMapping("/save")
+    public String saveVehicle(@ModelAttribute @Valid Vehicle vehicle,
+                              BindingResult bindResult) {
+        if (bindResult.hasErrors())
+            return "vehicles/vehicle-form";
+        else {
+            vehicleService.save(vehicle);
+            return "redirect:/vehicles/list-vehicles";
+        }
     }
 }
